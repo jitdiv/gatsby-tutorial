da838d1b9149bfdaf654d9163d14adcbe3e8502c
jtdv <jtdv.info@gmail.com>
2022-10-20 11:15:07 -0700

    Added styling for projects

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,23 @@
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from "../../components/Layout"
-import {portfolio} from "../../styles/projects.module.css"
+import {portfolio, project} from "../../styles/projects.module.css"
 
 export default function Projects({ data }) {
   console.log(data)
+  const projects = data.allMarkdownRemark.nodes;
   return (
     <Layout> 
         <div className={portfolio}>
         <h2>Portfolio</h2>
+        </div>
+        <div className={project}>
+          {projects.map(project => 
+            <Link to={"projects" + project.frontmatter.slug} key={project.id}>
+              <h3>{project.frontmatter.title}</h3>
+              <p>{project.frontmatter.stack}</p>
+            </Link>
+            )}
         </div>
     </Layout>
   )
