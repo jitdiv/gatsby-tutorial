aee0dd7e453ce74ae4c2a789c962e362295c4707
JTDV <jtdv.info@gmail.com>
2022-10-24 16:16:57 -0700

    Specified options for createPage

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 exports.createPages = async ({ graphql, actions }) => {
   const { data } = await graphql(`
       query Projects {
@@ -10,4 +12,14 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
+
+    data.allMarkdownRemark.nodes.forEach(node => {
+      actions.createPage({
+        path: '/projects/' + node.frontmatter.slug,
+        component: path.resolve('./src/templates/project-details.js'),
+        context: { slug: node.frontmatter.slug }
+      })
+    })
+
+
 }
